0d66890bbce9cd1e3cc4d795fbbffba8d48cab53
mainishanhoon <NishanKashyap@hotmail.com>
2025-05-25 00:04:31 +0530

    fix: adjust layout of loading skeletons for improved structure and consistency

--- src/app/home/item/[itemId]/loading.tsx
+++ src/app/home/item/[itemId]/loading.tsx
@@ -2,9 +2,10 @@ import { Skeleton } from '@/app/_components/ui/skeleton';
 
 export default function ProductLoadingRoute() {
   return (
-    <div className="grid gap-5 xl:grid-cols-2 p-2 md:p-4">
+    <div className="grid gap-5 p-2 md:p-4 xl:grid-cols-2">
       <div className="flex flex-col items-center justify-center">
         <Skeleton className="md:size-96" />
+
         <span className="mt-4 flex flex-wrap justify-center gap-3">
           <Skeleton className="size-16 md:size-24" />
           <Skeleton className="size-16 md:size-24" />
@@ -16,6 +17,10 @@ export default function ProductLoadingRoute() {
       <div>
         <Skeleton className="h-12 w-56" />
         <Skeleton className="mt-4 h-12 w-36" />
+        <div className="grid h-10 w-full grid-cols-2 gap-2">
+          <Skeleton className="size-full rounded-md" />
+          <Skeleton className="size-full rounded-md" />
+        </div>
         <Skeleton className="mt-4 h-60 w-full" />
         <Skeleton className="mt-5 h-12 w-full" />
       </div>
